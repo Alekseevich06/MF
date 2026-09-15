3174345e0f7e991c5d6842b2b70c4f45b6e3e8da
RomanTestWork <yakovlev.rma@yandex.ru>
2026-09-15 21:42:03 +0300

    test ci

--- scripts/build-mf.sh
+++ scripts/build-mf.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-export MF_BUILD_VERSION=$(git rev-parse --short HEAD)
-export MF_BUILD_NAME=$(git rev-parse --abbrev-ref HEAD)
+export MF_BUILD_VERSION="${MF_BUILD_VERSION:-$(git rev-parse --short HEAD)}"
+export MF_BUILD_NAME="${MF_BUILD_NAME:-$(git rev-parse --abbrev-ref HEAD)}"
 
 echo "Building MF with version=$MF_BUILD_VERSION, name=$MF_BUILD_NAME"
 
